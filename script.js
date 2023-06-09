9fec91a441dd64521771708a286a74d786ed4b59
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-09 17:26:10 -0300

    Acessar promise, apresentar erro, fim de processo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
+    .then(resposta => resposta.json())
+    .then(r => {
         if (r.erro) {
             throw Error('Esse CEP não existe!')
         } else 
             console.log(r)
-        })
-        .catch(erro => console.log(erro));
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamaneto concluído!'));
         
-
-console.log(consultaCEP);
+console.log(consultaCEP)
