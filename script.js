4b25258361f7a063ce656770bbb64e1f85716736
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-09 18:20:28 -0300

    Requisição através do try...catch

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse CEP não existe!')
-        } else 
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamaneto concluído!'));
-        
-console.log(consultaCEP)
+async function buscaEndereco() {
+    try {
+        var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }        
+        console.log(consultaCEPConvertida);
+    }   catch (erro) {
+        console.log(erro)
+    }
+} 
+
+buscaEndereco();
