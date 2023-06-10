ee09372c28662fe87bee0cf07a255721bac8f6a4
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-10 16:43:51 -0300

    Armazena valores convertidos nos campos  endereco

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
-        }        
+        }     
+        
+        var estado = document.getElementById('estado');
+        var cidade = document.getElementById('cidade');
+        var bairro = document.getElementById('bairro');
+        var logradouro = document.getElementById('logradouro');
+
+        estado.value = consultaCEPConvertida.uf;
+        cidade.value = consultaCEPConvertida.localidade;
+        bairro.value = consultaCEPConvertida.bairro;
+        logradouro.value = consultaCEPConvertida.logradouro;
+
         console.log(consultaCEPConvertida);
     }   catch (erro) {
         console.log(erro)
     }
 } 
 
-buscaEndereco();
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
