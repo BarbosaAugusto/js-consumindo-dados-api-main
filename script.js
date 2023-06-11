7f8a9dd7176726e31834d888aa03e37354e461c0
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-06-11 17:41:11 -0300

    Correção de código

--- script.js
+++ script.js
@@ -6,25 +6,22 @@ async function buscaEndereco(cep) {
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
-        }     
-        
-        var estado = document.getElementById('estado');
+        }
         var cidade = document.getElementById('cidade');
-        var bairro = document.getElementById('bairro');
-        var logradouro = document.getElementById('logradouro');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
 
-        estado.value = consultaCEPConvertida.uf;
         cidade.value = consultaCEPConvertida.localidade;
-        bairro.value = consultaCEPConvertida.bairro;
         logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
 
         console.log(consultaCEPConvertida);
-        return  consultaCEPConvertida;
-    }   catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido.Tente novamente!</p>`
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
         console.log(erro);
     }
-} 
+}
 
 var cep = document.getElementById('cep');
 cep.addEventListener("focusout", () => buscaEndereco(cep.value));
